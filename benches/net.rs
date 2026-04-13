8f8f1e66b5c3d25a37d2780a9a5db9c218fe06f3
Evgeny Safronov <division494@gmail.com>
2026-04-13 15:34:58 +0300

    feat: count specialization for addrs iterators (#25)

--- benches/net.rs
+++ benches/net.rs
@@ -75,13 +75,38 @@ fn bench_net_addrs(c: &mut Criterion) {
 fn bench_net_addrs_count(c: &mut Criterion) {
     let mut group = c.benchmark_group("netip");
 
-    group.throughput(Throughput::Elements(256));
     group.bench_function("Ipv4Network::addrs count /24", |b| {
         let net = Ipv4Network::parse("77.88.55.0/24").unwrap();
         b.iter(|| {
             core::hint::black_box(net.addrs().count());
         });
     });
+
+    group.bench_function("Ipv6Network::addrs count /120", |b| {
+        let net = Ipv6Network::parse("2a02:6b8:c00::1234:0:0/120").unwrap();
+        b.iter(|| {
+            core::hint::black_box(net.addrs().count());
+        });
+    });
+
+    group.bench_function("Ipv6Network::addrs count /112", |b| {
+        let net = Ipv6Network::parse("2a02:6b8:c00::1234:0:0/112").unwrap();
+        b.iter(|| {
+            core::hint::black_box(net.addrs().count());
+        });
+    });
+
+    group.bench_function("Ipv6Network::addrs count non-contiguous 8 host bits", |b| {
+        let net = Ipv6Network::new(
+            Ipv6Addr::new(0x2a02, 0x6b8, 0xc00, 0, 0, 0x1234, 0, 0),
+            Ipv6Addr::new(0xffff, 0xffff, 0xff00, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff),
+        );
+        b.iter(|| {
+            core::hint::black_box(net.addrs().count());
+        });
+    });
+
+    group.finish();
 }
 
 fn bench_intersection(c: &mut Criterion) {
